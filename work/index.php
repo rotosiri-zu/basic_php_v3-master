1e71445ddaf7ca43b028c399ed8aad2e87a47a1f
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-06-12 19:17:03 +0900

    【試験運用中】はじめてのPHP [macOS編] » #10 ファイルを読み込んでみよう

--- work/index.php
+++ work/index.php
@@ -1,4 +1,8 @@
+<?php
 
+include('_header.php');
+
+?>
     <section>
       <img src="img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
       <h1>勇者ゲーム</h1>
@@ -19,5 +23,7 @@
       <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
       <a href="memory.php">もっと見る</a>
     </section>
+<?php
 
-  
+include('_footer.php');
+
